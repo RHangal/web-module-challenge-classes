319f9094102bb33b2da2ce81eb6b937442a88c0a
Rohan Hangal <rohanhangal@gmail.com>
2022-11-15 11:15:20 -0800

    finished all tasks, now going to do the stretch

--- index.js
+++ index.js
@@ -212,8 +212,18 @@ class Student extends Lambdasian {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor{
+  constructor(PMProps){
+    super(PMProps);
+    this.gradClassName = PMProps.gradClassName
+    this.favInstructor = PMProps.favInstructor
+  }
+standUp(channel) {
+  return `${this.name} announces to ${channel}, @${channel} standy times!`
+}
+debugsCode(student, subject){
+  return `${this.name} debugs ${student.name}'s code on ${subject}`
+} 
 }
 
 /*
